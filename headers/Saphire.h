53088480a0036f09ba4838b842398a795c2809ed
anthropomorphic <anthropomorphic@mikefdorst.com>
2013-12-04 16:01:53 -0500

    Replace Number with Integer and Real

--- headers/Saphire.h
+++ headers/Saphire.h
@@ -19,39 +19,14 @@ public:
   }
 };
 
-class Number : id {
-  bool _is_int;
-  union {
-    long integer;
-    double real;
-  } val;
+class Integer : id {
 public:
-  class InvalidAccessException : std::exception {};
-  bool is_int() {
-    return _is_int;
-  }
-  void set_long(long i) {
-    val.integer = i;
-    _is_int = true;
-  }
-  void set_double(double d) {
-    val.real = d;
-    _is_int = false;
-  }
-  long get_long() {
-    if (_is_int) {
-      return val.integer;
-    } else {
-      throw(new InvalidAccessException);
-    }
-  }
-  double get_double() {
-    if (!_is_int) {
-      return val.real;
-    } else {
-      throw(new InvalidAccessException);
-    }
-  }
+  long val;
+};
+
+class Real {
+public:
+  double val;
 };
 
 class NilClass : id {
